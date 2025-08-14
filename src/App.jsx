479716a1af80c14708f9ff7c8e7ba06420734350
rev1s0r <petrik.baca@gmail.com>
2025-08-14 21:40:29 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Accordeon from './components/Accordeon'
-
+import RandomColor from './components/Random-color'
+import StarRating from './components/Star-rating'
 function App() {
 
   return (
     <>
     <div>
-      { <Accordeon /> }
+       <Accordeon /> 
+       <RandomColor /> 
+       <StarRating noOfStars={10} />
     </div>
       
     </>
